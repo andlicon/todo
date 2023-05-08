c616cc4e9d47fe5dab692ecbc90ddd868e97b767
andlicon <andresjzabalac@hotmail.com>
2023-05-08 10:30:42 -0400

    ADD: todo status FIX: bordes del todo

--- src/js/component/Todo.jsx
+++ src/js/component/Todo.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../styles/todo.css';
 
 const Todo = () => {
+  const [items, setItems] = useState([]);
+
+  const cantidadItems = items.length;
   return(
     <div className='todo'>
-
+      <div className='todo__adder'></div>
+      <div className='todo__body'></div>
+      <div className='todo__status'>
+        <p className='status'>
+          {
+            cantidadItems==0 
+            ? 'No task, add a task' 
+            : `${cantidadItems} item left`
+          }
+        </p>
+      </div>
     </div>
   );
 };
