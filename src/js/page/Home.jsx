3b7e3ff580a218fd12e113425654a1c8ca5e686d
andlicon <andresjzabalac@hotmail.com>
2023-05-08 09:10:48 -0400

    ADD: home posicionamiento

--- src/js/page/Home.jsx
+++ src/js/page/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import '../../styles/home.css';
 
 //create your first component
 const Home = () => {
 	return (
-		<div className='todo'>
-
+		<div className='container todo-container'>
+      aa
 		</div>
 	);
 };
