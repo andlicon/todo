6d7db71d3c9444496a1b4aa7b3f4056a15cb67b7
andlicon <andresjzabalac@hotmail.com>
2023-05-08 11:51:05 -0400

    ADD: se anadio funcionalidad para anadir items

--- src/js/component/Todo.jsx
+++ src/js/component/Todo.jsx
@@ -6,21 +6,43 @@ const Todo = () => {
   // hooks
   const [adder, setAdder] = useState('');
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
 
-  const handlerAdder = ({target}) => {
-    setAdder(target.value);
+  const handlerChange = (event) => {
+    setAdder(event.target.value);
+  }
+
+  const handlerOnKey = ({code}) => {
+    if( code=='Enter' ) anadirItem();
+  }
+
+  const anadirItem = () => {
+    if( adder==='' ) {
+      setError('No se puede cargar un item vacío');
+      return;
+    }
+    if( items.some( element => element==adder ) ) {
+      setError('No se pueden duplicar las tareas');
+      return;
+    }
+
+    setItems([...items, adder]);
+    setAdder('');
   }
 
   return(
     <div className='todo'>
       {/* anadir informacion al todo */}
-      <div className='todo__adder content-center'>
+      <div 
+        className='todo__adder content-center'
+        onKeyPress={handlerOnKey}
+      >
         <input 
           type='text'
           className='todo__item todo__input'
           id='adder'
           name='adder'
-          onChange={(handlerAdder)}
+          onChange={(handlerChange)}
           value={adder}
         />
       </div>
@@ -32,10 +54,17 @@ const Todo = () => {
           {
             items.length == 0 
             ? 'No task, add a task' 
-            : `${cantidadItems} item left`
+            : `${items.length} item left`
           }
         </p>
       </div>
+      {/* error */}
+      {
+        error != null
+        && <div className='error'>
+            <p className='error__mensaje'>{error}</p>
+          </div>
+      }
     </div>
   );
 };
