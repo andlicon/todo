7d05e3e8e01b1b24857e35c10df1d83dbbead6d4
andlicon <andresjzabalac@hotmail.com>
2023-05-08 09:40:57 -0400

    ADD: estilos al container todo

--- src/js/component/Todo.jsx
+++ src/js/component/Todo.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import '../../styles/todo.css';
+
 const Todo = () => {
   return(
-    <>
-    </>
+    <div className='todo'>
+
+    </div>
   );
 };
 export default Todo;
