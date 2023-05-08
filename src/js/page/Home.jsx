99a221ec355ff188e19ef0c7409f1d5587627428
andlicon <andresjzabalac@hotmail.com>
2023-05-08 09:18:54 -0400

    ADD: todo component

--- src/js/page/Home.jsx
+++ src/js/page/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import '../../styles/home.css';
+// componentes
+import Todo from '../component/Todo.jsx';
 
 //create your first component
 const Home = () => {
 	return (
 		<div className='container todo-container'>
+      {/* title */}
       <h1 className='main-title'>
         todos
       </h1>
-      
+      {/* content */}
+      <Todo />
 		</div>
 	);
 };
